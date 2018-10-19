c739b4555e6eec87bce73895ee48f099f16828ae
pinturavi <pintu541796@gmail.com>
2018-10-19 11:18:03 +0530

    changed setup info

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="CS50",
+    name="example_pkg_ravi",
     version="0.0.1",
     author="pintu ravi",
     author_email="pintu541796@gmail.com",
